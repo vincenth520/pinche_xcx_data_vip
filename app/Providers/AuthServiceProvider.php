ff88461593a8efadf136c21159332f9b9d1dbde5
vincenth520 <1091986039@qq.com>
2017-11-28 16:43:14 +0800

    update api passport time

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -29,7 +29,7 @@ class AuthServiceProvider extends ServiceProvider
 
         Passport::routes();
 
-        Passport::tokensExpireIn(Carbon::now()->addMinutes(30));
+        Passport::tokensExpireIn(Carbon::now()->addDays(7));
 
         Passport::refreshTokensExpireIn(Carbon::now()->addDays(10));
     }
